236fb64d12ceb25c39f7b5d9932c5f820ac645af
dgsahethi <dgsahethi@gmail.com>
2021-06-22 02:19:33 +0530

    Basic CRUD Infrastructure

--- app.js
+++ app.js
@@ -23,7 +23,9 @@ const app = express();
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
 
-app.use(express.urlencoded({ extended: true }))
+app.use(express.urlencoded({ extended: true }));
+
+app.use(methodOverride("_method"));
 
 app.get("/", (req, res) => {
     res.render("home");
@@ -54,6 +56,17 @@ app.get("/campgrounds/:id/edit", async (req, res) => {
     res.render("campgrounds/edit", { campground });
 })
 
+app.put("/campgrounds/:id", async (req, res) => {
+    const { id } = req.params;
+    const campground = await Campground.findByIdAndUpdate(id, { ...req.body.campground })
+    res.redirect(`/campgrounds/${campground._id}`)
+})
+
+app.delete("/campgrounds/:id", async (req, res) => {
+    const { id } = req.params;
+    await Campground.findByIdAndDelete(id);
+    res.redirect('/campgrounds');
+})
 
 app.listen(3000, () => {
     console.log("Serving on port 3000");
