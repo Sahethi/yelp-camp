4786d4a56aa5d79454c6fa65879c9d3d5c9b4e16
dgsahethi <dgsahethi@gmail.com>
2021-07-03 04:23:57 +0530

    Added Authentication

--- middleware.js
+++ middleware.js
@@ -1,5 +1,8 @@
 module.exports.isLoggedIn = (req, res, next) => {
+    console.log("REQ.USER...", req.user);
     if (!req.isAuthenticated()) {
+        //store the URL they are requesting 
+        req.session.returnTo = req.originalUrl
         req.flash('error', 'You must be signed in first!');
         return res.redirect('/login');
     }
