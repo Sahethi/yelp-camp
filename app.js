2f7efd5bc2bbb857445f4521f1d1654127380b58
dgsahethi <dgsahethi@gmail.com>
2021-09-03 20:45:39 +0530

    minor

--- app.js
+++ app.js
@@ -36,6 +36,8 @@ const reviewsRoutes = require('./routes/reviews');
 
 const dbUrl = process.env.DB_URL || 'mongodb://localhost:27017/yelp-camp';
 
+// const dbUrl = 'mongodb://localhost:27017/yelp-camp';
+
 mongoose.connect(dbUrl, {
     useNewUrlParser: true,
     useCreateIndex: true,
